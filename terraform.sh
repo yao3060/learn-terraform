fde9ebe60577b09ac13d6fc8d383026192a46d50
Yao Yingying <yaoyingying@Dings-Mac-mini.local>
2021-07-15 17:51:57 +0800

    add env check

--- terraform.sh
+++ terraform.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -f .env ]; then
+  echo -e '\033[0;31m'
+  echo ".env file is not exist."
+  echo "Please do 'cp .env.default .env'"
+  echo "Then fill the variables."
+  exit 1
+fi
+
 docker run --rm -ti \
   -e "TF_ROOT=/project" \
   --env-file .env \
